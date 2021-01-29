6dda97f7c82700d46f385e92f62048fdd0640bac
CraigSmith789 <craig789@gmail.com>
2021-01-29 17:09:56 -0500

    save

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const addTaskBtn = document.getElementById('new-button')
+  const taskList = document.getElementById('tasks')
+  addTaskBtn.addEventListener('click', handleSubmit)
+
+
+
+  function handleSubmit(event){
+    console.log('in handle submit function');
+    event.preventDefault();
+    const task = document.getElementById("new-task-description");
+    const li = document.createElement("li");
+    li.innerText = task.value
+    taskList.append(li)
+    document.getElementById("new-task-description").value = "";
+
+    
+
+
+    
+
+}
 });
